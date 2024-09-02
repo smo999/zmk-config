517bb05536bbbbf05ef328fc1746d8d4041bb346
Sébastien Mouthuy <>
2024-09-02 17:34:31 +0200

    - using the antecedent morph for the implemetation of hands down - ability to invert hands down - removing encoders

--- config/key_names.h
+++ config/key_names.h
@@ -7,34 +7,10 @@
 #define THUMBS LH0 LH1 RH1 RH0                                      // thumb keys
 
 // my layers
-#define	l_default		0
-#define	l_alpha		1
-#define	l_akG		2
-#define	l_akM		3
-#define	l_akX		4
-#define	l_akT		5
-#define	l_akC		6
-#define	l_akK		7
-#define	l_akD		8
-#define	l_akW		9
-#define	l_akF		10
-#define	l_akP		11
-#define	l_akB		12
-#define	l_akV		13
-#define	l_akJ		14
-#define	l_akA		15
-#define	l_akU		16
-#define	l_akE		17
-#define	l_akO		18
-#define	l_akI		19
-#define	l_akDot		20
-#define	l_symbols		21
-#define	l_numbers		22
-#define	l_nav2		23
-#define	l_nav1		24
-#define	l_config		25
-#define	l_writing_symbols		26
+#define	l_alpha		0
+#define	l_sym		1
+#define	l_num		2
+#define	l_nav2		3
+#define	l_nav1		4
+#define	l_conf		5
 		
-// define all the alpha layers of the hands down layout 
-#define ls_hd l_alpha l_akG l_akM l_akX l_akT l_akC l_akK l_akD l_akW l_akF l_akP l_akB l_akV l_akJ l_akA l_akU l_akE l_akO l_akI l_akDot
-
